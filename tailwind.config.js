4e510f04cdec51139e040a9d0d93d896c1126ba4
Michelle Nguyen <michellenguyen@ufis-13.lan>
2024-11-27 00:36:27 -0700

    Initializing repo

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'], // Default font for body text
+        heading: ['Poppins', 'sans-serif'], // Use Poppins for headings
+        serif: ['Lora', 'serif'], // Optional serif for accents
+      },
+    },
+  },
+  plugins: [],
+};
