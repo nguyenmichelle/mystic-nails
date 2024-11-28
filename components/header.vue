31661f4c23cced3c5a006a9876c2174b091b28ba
Michelle Nguyen <michellenguyen@ufis-13.lan>
2024-11-27 21:35:18 -0700

    Removing upper right menu from nav

--- components/header.vue
+++ components/header.vue
@@ -1,5 +1,5 @@
 <template>
-  <Disclosure as="nav" class="bg-white-800" v-slot="{ open }">
+  <Disclosure as="nav" class="bg-white-800">
     <div class="mx-auto max-w-7xl px-5">
       <div class="flex h-16 items-center justify-between">
         <!-- Left Section -->
@@ -10,22 +10,11 @@
         </div>
 
         <!-- Center Section -->
-        <div class="flex-1 flex items-center justify-center">
+        <div class="flex-1 flex items-right justify-end">
           <a href="/" class="flex items-center space-x-2">
             <img class="h-8 w-auto" src="https://res.cloudinary.com/dl0bohozg/image/upload/v1732662221/mystic-logo_rwqmil.png" alt="Mystic Nails">
             <span class="text-xs sm:text-base md:text-xl lg:text-2xl font-bold font-heading text-black">MYSTIC NAILS & DAY SPA</span>
           </a>
-        </div>
-
-        <!-- Right Section -->
-        <div class="flex-1 flex items-right justify-end hidden md:block">
-          <div class="text-right">
-            <a v-for="item in navigation" :key="item.name" :href="item.href"
-               :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-500 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium']"
-               :aria-current="item.current ? 'page' : undefined">
-              {{ item.name }}
-            </a>
-          </div>
         </div>
       </div>
     </div>
