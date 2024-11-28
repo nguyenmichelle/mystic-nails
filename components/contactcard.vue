bf96940bfe0f20d0ea510dd761f5405aa27e991d
Michelle Nguyen <michellenguyen@ufis-13.lan>
2024-11-27 21:32:17 -0700

    Updated styling of hours boxes and contact us cards

--- components/contactcard.vue
+++ components/contactcard.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="mx-10 my-2">
+  <div class="mx-10 mt-2">
     <div class="flex items-center justify-between border-2 border-purple-500 p-4 rounded-lg h-full">
       <div class="flex items-center space-x-2 w-full">
         <div class="flex-shrink-0 w-[10%] border-r-2 border-purple-600">
           <slot/>
         </div>
-        <div class="w-[90%]">
+        <div class="w-[90%] pl-2">
           <a class="font-sans hover:cursor-pointer text-purple-600" v-html="text" :href="action" target="_blank"></a>
         </div>
       </div>
--- components/sections/hours.vue
+++ components/sections/hours.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Section :isOdd="false" title="HOURS" id="hours">
-      <div class="flex flex-1 justify-center space-x-5 md:text-lg">
+      <div class="flex flex-1 justify-center space-x-3 md:space-x-10 md:text-lg">
         <Card title="Monday - Saturday" content="10AM - 7PM"></Card>
         <Card title="Sunday" content="CLOSED"></Card>
       </div>
