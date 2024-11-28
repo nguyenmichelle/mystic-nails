25ef8aadcb989aab7369f8ef08d32b12b2d78e6b
Michelle Nguyen <michellenguyen@ufis-13.lan>
2024-11-27 19:41:55 -0700

    Updating styling of menus

--- components/sections/services.vue
+++ components/sections/services.vue
@@ -1,19 +1,21 @@
 <template>
   <div>
     <Section :isOdd="true" title="SERVICES & PRICING LIST" id="services">
-      <div class="pb-10 flex flex-col md:flex-row items-center justify-center space-y-4 md:space-y-0 md:space-x-4 w-full">
-        <div class="flex justify-center items-center w-full h-[100%] md:w-[45%] h-[500px] sm:h-[600px] md:h-[810px]">
+      <div class="pb-10 flex flex-col md:flex-row items-center justify-center space-y-4 h-auto md:space-y-0 md:space-x-4">
+        <!-- First PDF object -->
+        <div class="flex justify-center items-center w-full h-auto md:w-[48%]">
           <object
               data="mystic-nails-menu-1.pdf#view=FitH&toolbar=0&navpanes=0"
               type="application/pdf"
-              class="w-full h-full"
+              class="w-full h-[80vh] md:h-[960px]"
           ></object>
         </div>
-        <div class="flex justify-center items-center w-full h-[100%] md:w-[45%] h-[500px] sm:h-[600px] md:h-[810px]">
+        <!-- Second PDF object -->
+        <div class="flex justify-center items-center w-full h-auto md:w-[48%]">
           <object
               data="mystic-nails-menu-2.pdf#view=FitH&toolbar=0&navpanes=0"
               type="application/pdf"
-              class="w-full h-full"
+              class="w-full h-[80vh] md:h-[960px]"
           ></object>
         </div>
       </div>
