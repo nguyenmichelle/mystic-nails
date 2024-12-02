8e535a077f9aa9b094e7291fff9223eb15dc8386
Michelle Nguyen <michellenguyen@ufis-13.lan>
2024-12-01 23:43:24 -0700

    Updating menus to pngs instead of pdfs

--- components/footer.vue
+++ components/footer.vue
@@ -2,7 +2,7 @@
   <footer class="bg-white dark:bg-white-900 m-4">
     <div>
       <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8" />
-      <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© {{ currentYear }} <a href="https://flowbite.com/" class="hover:underline">Mystic Nails & Spa</a>. All Rights Reserved.</span>
+      <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© {{ currentYear }} <a class="hover:underline">Mystic Nails & Day Spa</a>. All Rights Reserved.</span>
     </div>
   </footer>
 </template>
--- components/sections/services.vue
+++ components/sections/services.vue
@@ -2,21 +2,11 @@
   <div>
     <Section :isOdd="true" title="SERVICES & PRICING LIST" id="services">
       <div class="pb-10 flex flex-col md:flex-row items-center justify-center space-y-4 h-auto md:space-y-0 md:space-x-4">
-        <!-- First PDF object -->
         <div class="flex justify-center items-center w-full h-auto md:w-[48%]">
-          <object
-              data="mystic-nails-menu-1.pdf#view=FitH&toolbar=0&navpanes=0"
-              type="application/pdf"
-              class="w-full h-[80vh] md:h-[960px]"
-          ></object>
+          <img src="/mystic-nails-menu-pg-1.png" alt="Mystic Nails Menu 1" class="w-full h-[80vh] md:h-[960px]" />
         </div>
-        <!-- Second PDF object -->
         <div class="flex justify-center items-center w-full h-auto md:w-[48%]">
-          <object
-              data="mystic-nails-menu-2.pdf#view=FitH&toolbar=0&navpanes=0"
-              type="application/pdf"
-              class="w-full h-[80vh] md:h-[960px]"
-          ></object>
+          <img src="/mystic-nails-menu-pg-2.png" alt="Mystic Nails Menu 2" class="w-full h-[80vh] md:h-[960px]"/>
         </div>
       </div>
     </Section>
@@ -25,19 +15,5 @@
 
 <script>
 export default {
-  methods: {
-    expandToFullScreen() {
-      const iframe = this.$refs.iframe;
-      if (iframe && iframe.requestFullscreen) {
-        iframe.requestFullscreen(); // Enter fullscreen mode
-      } else if (iframe && iframe.webkitRequestFullscreen) {
-        iframe.webkitRequestFullscreen(); // Safari support
-      } else if (iframe && iframe.msRequestFullscreen) {
-        iframe.msRequestFullscreen();
-      } else {
-        alert("Fullscreen mode is not supported in your browser.");
-      }
-    },
-  },
 };
 </script>
