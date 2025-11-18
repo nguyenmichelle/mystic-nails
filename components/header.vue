264ab50dca581c5841af3803b5c307b82742090f
Michelle Nguyen <michellenguyen@ufis-13.lan>
2025-11-17 17:22:09 -0700

    Adding booking link to navbar

--- components/header.vue
+++ components/header.vue
@@ -1,11 +1,17 @@
 <template>
-  <Disclosure as="nav" class="bg-white-800">
+  <disclosure as="nav" class="bg-white">
     <div class="mx-auto max-w-7xl px-5">
       <div class="flex h-16 items-center justify-between">
         <!-- Left Section -->
         <div class="flex-1 flex items-center justify-start">
-          <a href="tel:+18013027277" class="text-black font-medium font-sans hover:underline">
-            (801) 302-7277
+          <a
+              href="https://www.vagaro.com/mysticnail"
+              target="_blank"
+              class="rounded-md border-2 border-gray-400 px-3.5 py-2.5 text-sm font-semibold text-gray-700 shadow-sm
+                   hover:bg-purple-800 hover:text-white
+                   focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
+          >
+            BOOK NOW
           </a>
         </div>
 
@@ -13,23 +19,13 @@
         <div class="flex-1 flex items-right justify-end">
           <a href="/" class="flex items-center space-x-2">
             <img class="h-8 w-auto" src="https://res.cloudinary.com/dl0bohozg/image/upload/v1732662221/mystic-logo_rwqmil.png" alt="Mystic Nails">
-            <span class="text-xs sm:text-base md:text-xl lg:text-2xl font-bold font-heading text-black">MYSTIC NAILS & DAY SPA</span>
+            <span class="text-xs sm:text-base md:text-xl lg:text-2xl font-bold font-heading text-black">
+              MYSTIC NAILS & DAY SPA
+            </span>
           </a>
         </div>
       </div>
     </div>
     <hr>
-  </Disclosure>
+  </disclosure>
 </template>
-
-<script setup>
-import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
-
-const navigation = [
-  { name: 'Services', href: '/#services', current: false },
-  { name: 'Our Hours', href: '/#hours', current: false },
-  { name: 'Reviews', href: '/#reviews', current: false },
-  { name: 'Contact Us', href: '/#contact', current: false },
-]
-</script>
